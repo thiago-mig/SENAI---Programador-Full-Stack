e695adc9c828f19f5ac23e9d7d89a89af54de7ed
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2023-10-24 10:58:46 -0300

    adicionado o componente menu, com o toolbar do angular material e adicionado o modulo de icons do material

--- front-end/uc-interacao-com-apis/atividade-04a-sp1-iteracao-com-apis/LOJA-GAMES/src/app/app.module.ts
+++ front-end/uc-interacao-com-apis/atividade-04a-sp1-iteracao-com-apis/LOJA-GAMES/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Imports dos componentes Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
