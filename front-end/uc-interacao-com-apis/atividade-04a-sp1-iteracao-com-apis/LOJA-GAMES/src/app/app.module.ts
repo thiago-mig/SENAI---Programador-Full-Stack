db94218a3d6d1e330e557a5ac4aba6377eadd828
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2023-10-25 13:13:44 -0300

    Realizado a implementação do componente inicio com o carrosel e o grid de cards

--- front-end/uc-interacao-com-apis/atividade-04a-sp1-iteracao-com-apis/LOJA-GAMES/src/app/app.module.ts
+++ front-end/uc-interacao-com-apis/atividade-04a-sp1-iteracao-com-apis/LOJA-GAMES/src/app/app.module.ts
@@ -4,23 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 //Imports dos componentes Angular Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './components/menu/menu.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
